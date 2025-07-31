5803ddffaade006b324a86eb0864562c91763f85
Shogant <shogantanya@gmail.com>
2025-07-31 17:06:56 +0300

    desing - cloumn size adjust

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def get_weather(city,api_key, temp_selection, speed_selection):
             st.success(f"Weather for {city.title()}, {country}")
             st.write(f"📅 **Date:** {datetime.date.today().strftime('%A, %d %B %Y')}")
 
-            col1, col2, col3 = st.columns([1, 3, 1])
+            col1, col2, col3 = st.columns([1, 1, 1])
 
             with col1:
                 st.image(icon_url)
